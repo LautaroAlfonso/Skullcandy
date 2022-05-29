facc92246d8d4691eae5fe631333d50ac272a332
Lautaro Alfonso <inside@inside.inside1>
2022-05-29 18:54:30 -0300

    agregado a git

---
+++ index.js
@@ -0,0 +1,72 @@
+// let numero=1
+
+// for (let index = 0; index <=12; index++) {
+    
+//     let resultado = numero * index
+//     console.log('resultado: ' + index + '*' + numero + "=" + resultado)
+    
+//     if(index==9){
+//         continue;
+//     }
+
+//     if(index == 12){
+//         break;
+//     }
+    
+
+
+// }
+
+
+// let number=2
+
+// for (let y= 0; y <=20; y++) {
+    
+//     let resultado = number * y
+//     console.log( "resultado: " + y + "*" +  number + "=" + resultado )
+
+//     if(resultado == 10 ){
+//         continue;
+//     }
+
+//     if(resultado == 40 ){
+//         break;
+//     }
+
+
+
+    
+// }
+
+
+
+let numero = 1
+
+for (let i = 1; i <=12; i++) {
+    resultado = i * numero
+
+    console.log("resultado :" + i + "*" + numero + "=" + resultado)
+    
+ 
+    for (let y = 1; y <=12; y++) {
+        resultado = y * numero
+        console.log("resultado :" + y + "*" + numero + "=" + resultado);
+    }
+
+    
+    
+    if( resultado = 12 * i){
+        numero++;
+        
+    }
+
+    
+    
+    
+}
+
+console.log("finalizado")
+
+
+
+

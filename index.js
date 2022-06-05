8a4daf1af2ca562522273c5592c301a77c949b5e
Lautaro Alfonso <inside@inside.inside1>
2022-06-05 12:34:23 -0300

    agregado de class

--- index.js
+++ index.js
@@ -40,33 +40,72 @@
 
 
 
-let numero = 1
+// let numero = 1
 
-for (let i = 1; i <=12; i++) {
-    resultado = i * numero
+// for (let i = 1; i <=12; i++) {
+//     resultado = i * numero
 
-    console.log("resultado :" + i + "*" + numero + "=" + resultado)
+//     console.log("resultado :" + i + "*" + numero + "=" + resultado)
     
  
-    for (let y = 1; y <=12; y++) {
-        resultado = y * numero
-        console.log("resultado :" + y + "*" + numero + "=" + resultado);
-    }
+//     for (let y = 1; y <=12; y++) {
+//          resultado = y * numero
+//         console.log("resultado :" + y + "*" + numero + "=" + resultado);
+//     }
 
     
     
-    if( resultado = 12 * i){
-        numero++;
+//      if( resultado = 12 * i){
+//          numero++;
         
-    }
+//      }
 
     
     
     
+//  }
+
+//  console.log("finalizado")
+
+
+
+
+class Product {
+    constructor (name, price, description){
+    this.name = name
+    this.price = price
+    this.description = description
+    }
+    calculatePriceIva(){
+    return(this.price) + ((this.price * IVA)/100)
+    }
 }
 
-console.log("finalizado")
+const IVA = 21
+
+const earbuds1 = new Product ("Grind", 8800, "True Wireless Earbuds")
+
+const earbuds2= new Product ("Pit Viper Push", 7000, "Active True Wireless Earbuds")
+
+const earbuds3= new Product ("Pit Viper", 8600, "True Wireless Earbuds")
+
+const earbuds4= new Product ("Indy", 6500, "Fuel True Wireless Earbuds")
+
+const earbuds5= new Product ("Indy ANC", 5500, "Noise Canceling True Wireless")
+
+const earbuds6= new Product("Budweiser Dime", 9000, "True Wireless Earbuds")
+
+console.log(earbuds1.name)
+
+console.log(earbuds1.price)
+
+console.log(earbuds1.calculatePriceIva())
 
 
+console.log(earbuds2.name)
+
+console.log(earbuds2.price)
+
+console.log(earbuds2.calculatePriceIva())
 
 

0488e0f9e7759ac02f91d70546cec1a2449dfc2c
Lautaro Alfonso <inside@inside.inside1>
2022-06-14 11:59:03 -0300

    agregado de js

--- index.js
+++ index.js
@@ -1,118 +1,4 @@
-// let numero=1
 
-// for (let index = 0; index <=12; index++) {
-    
-//     let resultado = numero * index
-//     console.log('resultado: ' + index + '*' + numero + "=" + resultado)
-    
-//     if(index==9){
-//         continue;
-//     }
-
-//     if(index == 12){
-//         break;
-//     }
-    
-
-
-// }
-
-
-// let number=2
-
-// for (let y= 0; y <=20; y++) {
-    
-//     let resultado = number * y
-//     console.log( "resultado: " + y + "*" +  number + "=" + resultado )
-
-//     if(resultado == 10 ){
-//         continue;
-//     }
-
-//     if(resultado == 40 ){
-//         break;
-//     }
-
-
-
-    
-// }
-
-
-
-// let numero = 1
-
-// for (let i = 1; i <=12; i++) {
-//     resultado = i * numero
-
-//     console.log("resultado :" + i + "*" + numero + "=" + resultado)
-    
- 
-//     for (let y = 1; y <=12; y++) {
-//          resultado = y * numero
-//         console.log("resultado :" + y + "*" + numero + "=" + resultado);
-//     }
-
-    
-    
-//      if( resultado = 12 * i){
-//          numero++;
-        
-//      }
-
-    
-    
-    
-//  }
-
-//  console.log("finalizado")
-
-
-
-
-class Product0 {
-    constructor (name, price, description){
-    this.name = name
-    this.price = price
-    this.description = description
-    }
-    calculatePriceIva(){
-    return(this.price) + ((this.price * IVA)/100)
-    }
-}
-
-const IVA = 21
-
-const earbuds1 = new Product0 ("Grind", 8800, "True Wireless Earbuds")
-
-const earbuds2= new Product0 ("Pit Viper Push", 7000, "Active True Wireless Earbuds")
-
-const earbuds3= new Product0 ("Pit Viper", 8600, "True Wireless Earbuds")
-
-const earbuds4= new Product0 ("Indy", 6500, "Fuel True Wireless Earbuds")
-
-const earbuds5= new Product0 ("Indy ANC", 5500, "Noise Canceling True Wireless")
-
-const earbuds6= new Product0("Budweiser Dime", 9000, "True Wireless Earbuds")
-
-console.log(earbuds1.name)
-
-console.log(earbuds1.price)
-
-console.log(earbuds1.calculatePriceIva())
-
-
-console.log(earbuds2.name)
-
-console.log(earbuds2.price)
-
-console.log(earbuds2.calculatePriceIva())
-
-
-function solicitarNombre(){
-    let nombre = prompt ("Ingrese su nombre")
-    alert("Nombre ingresado: " + nombre)
-}
 
 
 class carritoDeCompras{
@@ -128,6 +14,7 @@ class carritoDeCompras{
 
     getTotal() {
         for ( const producto of this.productos) {
+            console.log(producto)
         this.total = this.total + producto.price}
          console.log(this.total)
         }
@@ -155,3 +42,5 @@ class carritoDeCompras{
     cliente.addProduct(p5)
     cliente.addProduct(p6)
     cliente.getTotal()
+
+    

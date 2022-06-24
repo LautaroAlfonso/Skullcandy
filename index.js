3e8c953060ed1e3091fe50843b8de57e61077438
Lautaro Alfonso <inside@inside.inside1>
2022-06-24 12:54:12 -0300

    Agregado de storage y JSON

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+
+
+
 let showProducts = document.getElementById("showProducts")
 let showAllProducts = document.getElementById("showAllProducts")
 const div = document.querySelector(".div")
@@ -5,7 +10,106 @@ const div = document.querySelector(".div")
 let cart = []
 
 
+const Products = [
 
+    {
+        id: "1",
+
+        category: "Earbuds",
+
+        description: "True Wireless Earbuds",
+
+        name: "Grind®",
+
+        price: "8800",
+
+        img: "img/imagen2.png"
+
+        
+    },
+
+    {
+        id: "2",
+
+        category: "Earbuds",
+
+        description: " Active True Wireless Earbuds",
+
+        name: "Pit Viper Push™ ",
+
+        price: "7000",
+        
+        img: "img/imagen4.png"
+
+        
+    },
+
+    {
+        id: "3",
+
+        category: "Earbuds",
+
+        description: "True Wireless Earbuds",
+
+        name: "Pit Viper Grind®®",
+
+        price: "8600",
+        
+        img: "img/imagen5.png"
+
+        
+    },
+
+    {
+        id: "4",
+
+        category: "Airdots",
+
+        description: "Fuel True Wireless Earbuds",
+
+        name: "Indy™",
+
+        price: "6500",
+        
+        img: "img/imagen6.png"
+
+        
+    },
+
+    {
+        id: "5",
+
+        category: "Airdots",
+
+        description: " Noise Canceling True Wireless Earbuds",
+
+        name: "Indy™ ANC",
+
+        price: "5500",
+        
+        img: "img/imagen7.png"
+
+        
+    },
+
+    {
+        id: "6",
+
+        category: "Earbuds",
+
+        description: "Budweiser Dime® True Wireless Earbuds",
+
+        name: "Grind®",
+
+        price: "9000",
+        
+        img: "img/imagen8.png"
+
+        
+    },
+
+
+]
 
 
 
@@ -22,7 +126,7 @@ function mostrarProductos() {
         let price = document.createElement("p")
         price.innerText = (Product.price)
         let buyButton = document.createElement("button")
-        buyButton.innerText = ("Agregar al carrito")
+        buyButton.innerText = ("Agregar")
         card.append(img, name, price, buyButton)
 
         buyButton.addEventListener("click", function()
@@ -62,12 +166,13 @@ function showCart() {
         divCart.innerHTML += ` 
         <img src = "${Product.img}">
         <h3> ${Product.name}</h3>
-        <h3> ${Product.price}</h3>
+        <h3>$ ${Product.price}</h3>
        ` 
         
 
         div.appendChild(divCart)
-        
+
+       
     })
 
     let eliminarItem = document.querySelector(".eliminar")
@@ -75,9 +180,8 @@ function showCart() {
 
 
 
-
-
 // REDUCE PRECIO TOTAL DEL CARRITO //
+
 
 const total = cart.map((Product)=> parseInt(Product.price)).reduce((cartTotalPrice, currentProductPrice) => cartTotalPrice + currentProductPrice, 0);
 console.log(total);
@@ -85,7 +189,6 @@ console.log(total);
 let totalCompra = document.createElement("h4")
 totalCompra.innerText = ("Total: " + total)
 div.append(totalCompra)
-
 
 
 
@@ -101,6 +204,7 @@ div.append(deleteCart)
     div.innerHTML = ``
     console.log(cart) 
                             }
+                            localStorage.clear();
 }
 
 buttonCart.onclick = () => {
@@ -110,15 +214,15 @@ buttonCart.onclick = () => {
 }
 
 
-
-
-
 // Filtrar Productos //
 
 let buscador = document.getElementById("inputSearch")
 let filtrar = document.getElementById("filtrar")
+favoritos.addEventListener("click",favoritos);
 
 function filtrarPorCat(){
+
+    let favoritos = localStorage.getItem("favoritos")
     let verTodo = document.createElement("button")
     verTodo.innerText = ("Ver Todo")
     showProducts.append(verTodo)
@@ -159,64 +263,113 @@ filtrar.onclick = () => {
 }
 
 
+// Guardar Lista //
+
+let saveButton = document.getElementById("saveButton");
+saveButton.innerText = ("Guardaste el carrito")
+div.append(saveButton)
+
+function saveList(){
+
+    let  = [];
+   
+    for (let i = 0; i < ProductsList.children.length; i++) {
+        
+        let Products = ProductsList.children.item(i);
+        
+       
+        Products.push(Products)
+    }
+
+}
+saveList()
+
+localStorage.setItem("Products", JSON.stringify(Products))
 
 
 
 
+// Storage y JSON //
 
+function guardarCarritoEnLocalStorage(){
+
+    miLocalStorage.setItem("carrito", JSON.stringify(carrito))
+    
+    }
+    guardarCarritoEnLocalStorage();
+    
+    function cargarCarritoEnLocalStorage(){
+    
+        if(miLocalStorage.getItem("carrito") !==null){
+            carrito = JSON.parse(localStorage.getItem("carrito"))
+    
+        }
+    
+    }
+     
+    cargarCarritoEnLocalStorage();
+   
+   
+
+localStorage.setItem("miListaDeProductos", Products)
 
 // Clases carrito de compras//
 
 
-class carritoDeCompras{
-    constructor(){
-        this.productos = []
-        this.name = ""
-    }
-    setName(value){this.name = value}
-    addProduct(Product){this.productos.push(Product)}
-    removeLastProduct(){this.productos.pop()}
-    removeFirstProduct(){this.productos.shift()}
+// class carritoDeCompras{
+//     constructor(){
+//         this.productos = []
+//         this.name = ""
+//     }
+//     setName(value){this.name = value}
+//     addProduct(Product){this.productos.push(Product)}
+//     removeLastProduct(){this.productos.pop()}
+//     removeFirstProduct(){this.productos.shift()}
 
-    getTotal() {
-       let totalCalculado = 0;
-        for ( const producto of this.productos){
-            totalCalculado = totalCalculado + producto.price;
-        }
-         return totalCalculado;
-    }
-    getProductByName(Text) {
-        return this.productos.filter((El)=> El.nombre.includes(Text));
-      }
-}
+//     getTotal() {
+//        let totalCalculado = 0;
+//         for ( const producto of this.productos){
+//             totalCalculado = totalCalculado + producto.price;
+//         }
+//          return totalCalculado;
+//     }
+//     getProductByName(Text) {
+//         return this.productos.filter((El)=> El.nombre.includes(Text));
+//       }
+// }
 
-    class Product{
-        constructor(name, price){
-        this.name = name
-        this.price = price}
-    }
+//     class Product{
+//         constructor(name, price){
+//         this.name = name
+//         this.price = price}
+//     }
 
-    const cliente = new carritoDeCompras()
-    const p1 = new Product ("Grind", 8800)
-    const p2 = new Product ("Pit Viper Push", 7000)
-    const p3 = new Product ("Pit Viper", 8600)
-    const p4 = new Product ("Indy", 6500)
-    const p5 = new Product ("Indy ANC", 5500)
-    const p6 = new Product ("Budweiser Dime", 9000)
-
-
-    cliente.addProduct(p1)
-    cliente.addProduct(p2)
-    cliente.addProduct(p3)
-    cliente.addProduct(p4)
-    cliente.addProduct(p5)
-    cliente.addProduct(p6)
-    cliente.getTotal()
-
-    console.log(cliente.getTotal())
+//     const cliente = new carritoDeCompras()
+//     const p1 = new Product ("Grind", 8800)
+//     const p2 = new Product ("Pit Viper Push", 7000)
+//     const p3 = new Product ("Pit Viper", 8600)
+//     const p4 = new Product ("Indy", 6500)
+//     const p5 = new Product ("Indy ANC", 5500)
+//     const p6 = new Product ("Budweiser Dime", 9000)
 
 
+//     cliente.addProduct(p1)
+//     cliente.addProduct(p2)
+//     cliente.addProduct(p3)
+//     cliente.addProduct(p4)
+//     cliente.addProduct(p5)
+//     cliente.addProduct(p6)
+//     cliente.getTotal()
+
+//     console.log(cliente.getTotal())
 
 
 
 
+   
+//     // Inicio //
+//     cargarCarritoEnLocalStorage()
+
+// localStorage.setItem("listaDeProductos", JSON.stringify(Products))
+
+})
--- productos.js
+++ productos.js
@@ -1,100 +1,179 @@
-const Products = [
+// const Products = [
 
-    {
-        id: "1",
+//     {
+//         id: "1",
 
-        category: "Earbuds",
+//         category: "Earbuds",
 
-        description: "True Wireless Earbuds",
+//         description: "True Wireless Earbuds",
 
-        name: "Grind®",
+//         name: "Grind®",
 
-        price: "$8800",
+//         price: "8800",
 
-        img: "img/imagen2.png"
-
-        
-    },
-
-    {
-        id: "2",
-
-        category: "Earbuds",
-
-        description: " Active True Wireless Earbuds",
-
-        name: "Pit Viper Push™ ",
-
-        price: "$7000",
-        
-        img: "img/imagen4.png"
+//         img: "img/imagen2.png"
 
         
-    },
+//     },
 
-    {
-        id: "3",
+//     {
+//         id: "2",
 
-        category: "Earbuds",
+//         category: "Earbuds",
 
-        description: "True Wireless Earbuds",
+//         description: " Active True Wireless Earbuds",
 
-        name: "Pit Viper Grind®®",
+//         name: "Pit Viper Push™ ",
 
-        price: "$8600",
+//         price: "7000",
         
-        img: "img/imagen5.png"
-
-        
-    },
-
-    {
-        id: "4",
-
-        category: "Airdots",
-
-        description: "Fuel True Wireless Earbuds",
-
-        name: "Indy™",
-
-        price: "$6500",
-        
-        img: "img/imagen6.png"
+//         img: "img/imagen4.png"
 
         
-    },
+//     },
 
-    {
-        id: "5",
+//     {
+//         id: "3",
 
-        category: "Airdots",
+//         category: "Earbuds",
 
-        description: " Noise Canceling True Wireless Earbuds",
+//         description: "True Wireless Earbuds",
 
-        name: "Indy™ ANC",
+//         name: "Pit Viper Grind®®",
 
-        price: "$5500",
+//         price: "8600",
         
-        img: "img/imagen7.png"
-
-        
-    },
-
-    {
-        id: "6",
-
-        category: "Earbuds",
-
-        description: "Budweiser Dime® True Wireless Earbuds",
-
-        name: "Grind®",
-
-        price: "$9000",
-        
-        img: "img/imagen8.png"
+//         img: "img/imagen5.png"
 
         
-    },
+//     },
+
+//     {
+//         id: "4",
+
+//         category: "Airdots",
+
+//         description: "Fuel True Wireless Earbuds",
+
+//         name: "Indy™",
+
+//         price: "6500",
+        
+//         img: "img/imagen6.png"
+
+        
+//     },
+
+//     {
+//         id: "5",
+
+//         category: "Airdots",
+
+//         description: " Noise Canceling True Wireless Earbuds",
+
+//         name: "Indy™ ANC",
+
+//         price: "5500",
+        
+//         img: "img/imagen7.png"
+
+        
+//     },
+
+//     {
+//         id: "6",
+
+//         category: "Earbuds",
+
+//         description: "Budweiser Dime® True Wireless Earbuds",
+
+//         name: "Grind®",
+
+//         price: "9000",
+        
+//         img: "img/imagen8.png"
+
+        
+//     },
 
 
-]
+// ]
+
+
+// function guardarCarritoEnLocalStorage(){
+
+// localStorage.setItem("listaDeProductos", JSON.stringify(Products))
+
+// }
+
+// function cargarCarritoEnLocalStorage(){
+
+//     if(localStorage.getItem("listaDeProductos") !==null){
+//         Products = JSON.parse(localStorage.getItem("listaDeProductos"))
+
+//     }
+
+// }
+
+// // Inicio //
+// cargarCarritoEnLocalStorage()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Guardar Lista //
+
+// let saveButton = document.getElementById("saveButton");
+
+// saveButton.addEventListener("click", saveList);
+
+// function saveList(){
+
+//     alert("Guardaste el carrito")
+
+//     let Products = [];
+
+//     for (let i = 0; i < ProductsList.children.length; i++) {
+        
+//         let producto = ProductsList.children.item(i);
+        
+
+//         Products.push(producto)
+//     }
+
+localStorage.setItem("listaDeProductos", JSON.stringify(Products))
+
+   
+}
+
+// // Traer lista guardada //
+// function loadList(){
+
+//     if(localStorage.getItem("listaDeProductos") !=null){
+//         var Products = JSON.parse(localStorage.getItem("listaDeProductos"))
+
+//     }
+
+//     for (let i = 0; i < Products.length; i++) {
+
+//         let producto = Products (i);
+//         newProductsItem (producto)
+
+//     }
+
+// }
+
+// loadList()

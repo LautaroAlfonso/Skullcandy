4d4725bfe1ed3433d0b50003163f7445e353a785
Lautaro Alfonso <inside@inside.inside1>
2022-06-27 20:20:38 -0300

    mejoras en el carrito

--- index.js
+++ index.js
@@ -1,249 +1,174 @@
-document.addEventListener("DOMContentLoaded", () =>{
-
-
-
-
-let showProducts = document.getElementById("showProducts")
-let showAllProducts = document.getElementById("showAllProducts")
-const div = document.querySelector(".div")
-
-let cart = []
-
-
-
-// MOSTRAR PRODUCTOS //
-
-function mostrarProductos() {
-    Products.forEach((Product)=> {
-        let card = document.createElement("div")
-        showAllProducts.append(card)
-        let img = document.createElement("img")
-        img.setAttribute(  "src", Product.img)
-        let name = document.createElement("h3")
-        name.innerText = (Product.name)
-        let price = document.createElement("p")
-        price.innerText = (Product.price)
-        let btnAgregar = document.createElement("button")
-        btnAgregar.innerText = ("Agregar")
-        card.append(img, name, price, btnAgregar)
-
-        btnAgregar.addEventListener("click", function()
-        {
-            cart.push(Product)
-            alert ("Agregaste " + Product.name + " al carrito")
-            
-            div.innerHTML = ""
-            showCart()
-        })
-    })
-}
-mostrarProductos()
-
-
-
-
-// MOSTRAR CARRITO//
-
-let cartView = document.getElementById("showProductsCart")
-let goToCart = document.getElementById("goToCart")
-let carrito = document.getElementById ("mostrarCarrito")
-
-let alertCart = document.createElement("h2")
-alertCart.setAttribute = ("class", "alerta")
-
-if (!cart.length){
-    alertCart.innerText = ("El carrito esta vacio")
-    div.append(alertCart)
-}
-
-function showCart() {
-    alertCart.remove()
-
-    cart.forEach((Product)=>{
-        const divCart = document.createElement("li")
-        divCart.innerHTML += ` 
-        <img src = "${Product.img}">
-        <h3> ${Product.name}</h3>
-        <h3>$ ${Product.price}</h3>
-       ` 
-        
-
-        div.appendChild(divCart)
-
-       
-    })
-
-    let eliminarItem = document.querySelector(".eliminar")
-
-
-
-
-// REDUCE PRECIO TOTAL DEL CARRITO //
-
-
-const total = cart.map((Product)=> parseInt(Product.price)).reduce((cartTotalPrice, currentProductPrice) => cartTotalPrice + currentProductPrice, 0);
-console.log(total);
-
-let totalCompra = document.createElement("h4")
-totalCompra.innerText = ("Total: " + total)
-div.append(totalCompra)
-
-
-// Vaciar carrito //
-
- let deleteCart = document.createElement("button")
-deleteCart.innerText = ("Vaciar carrito")
-div.append(deleteCart)
-
- deleteCart.onclick = () => {
-    cart =[]
-    div.innerHTML = ``
-    console.log(cart) 
-                            }
-    localStorage.clear();
-}
-
-buttonCart.onclick = () => {
-    div.innerHTML = ``
-
-    showCart()
-}
-
-
-// Guardar Lista //
-let saveButton = document.getElementById("saveButton");
-
-// Storage y JSON //
-
-function guardarCarritoEnLocalStorage(){
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    saveButton.innerText = ("Guardaste el carrito")
-    saveButton.append(carrito)
-    }
-
-
-
-    
-function cargarCarritoEnLocalStorage(){
-    
-    if(localStorage.getItem("carrito") !==null){
-        carrito = JSON.parse(localStorage.getItem("carrito"))
-
-    }
-
-}
-
-localStorage.setItem("Products", JSON.stringify(Products))
-
-guardarCarritoEnLocalStorage();
-saveList()
-cargarCarritoEnLocalStorage();
-
-
-//Filtrar Productos //
-
-let buscador = document.getElementById("inputSearch")
-let filtrar = document.getElementById("filtrar")
-Products.addEventListener("click",favoritos);
-
-function filtrarPorCat(){
-
-    let Products = localStorage.getItem("favoritos")
-    let verTodo = document.createElement("button")
-    verTodo.innerText = ("Ver Todo")
-    showProducts.append(verTodo)
-    const filteredProducts = Products.filter((Product)=> Product.category === buscador.value)
-    console.log(filteredProducts)
-
-
-    filteredProducts.forEach((filter)=> {
-
-    let cardFilter = document.createElement("div")
-    showAllProducts.append(card)
-    let imgFilter = document.createElement("img")
-    imgFilter.setAttribute("src", Product.img)
-    let nameFilter = document.createElement("h3")
-    nameFilter.innerText = (Product.name)
-    let priceFilter = document.createElement("p")
-    priceFilter.innerText = (Product.price)
-    let buyButtonFilter = document.createElement("button")
-    buyButtonFilter.innerText = ("Agregar al carrito")
-    cardFilter.append(imgFilter, nameFilter, priceFilter, buyButtonFilter)
-    } )
-
-verTodo.onclick = () => {
-    showAllProducts.innerHTML = ``
-    mostrarProductos()
-}
-
-}
-
-buscador.onchange = () => {
-    showAllProducts.innerHTML = ``
-    filtrarPorCat()
-}
-
-filtrar.onclick = () => {
-    showAllProducts. innerHTML= ``
-    filtrarPorCat()
-}
-// Clases carrito de compras//
-
-
-// class carritoDeCompras{
-//     constructor(){
-//         this.productos = []
-//         this.name = ""
-//     }
-//     setName(value){this.name = value}
-//     addProduct(Product){this.productos.push(Product)}
-//     removeLastProduct(){this.productos.pop()}
-//     removeFirstProduct(){this.productos.shift()}
-
-//     getTotal() {
-//        let totalCalculado = 0;
-//         for ( const producto of this.productos){
-//             totalCalculado = totalCalculado + producto.price;
-//         }
-//          return totalCalculado;
-//     }
-//     getProductByName(Text) {
-//         return this.productos.filter((El)=> El.nombre.includes(Text));
-//       }
-// }
-
-//     class Product{
-//         constructor(name, price){
-//         this.name = name
-//         this.price = price}
-//     }
-
-//     const cliente = new carritoDeCompras()
-//     const p1 = new Product ("Grind", 8800)
-//     const p2 = new Product ("Pit Viper Push", 7000)
-//     const p3 = new Product ("Pit Viper", 8600)
-//     const p4 = new Product ("Indy", 6500)
-//     const p5 = new Product ("Indy ANC", 5500)
-//     const p6 = new Product ("Budweiser Dime", 9000)
-
-
-//     cliente.addProduct(p1)
-//     cliente.addProduct(p2)
-//     cliente.addProduct(p3)
-//     cliente.addProduct(p4)
-//     cliente.addProduct(p5)
-//     cliente.addProduct(p6)
-//     cliente.getTotal()
-
-//     console.log(cliente.getTotal())
-
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    let showProducts = document.getElementById("showProducts");
+    let showAllProducts = document.getElementById("showAllProducts");
+    const div = document.querySelector(".div");
+  
    
-//     // Inicio //
-//     cargarCarritoEnLocalStorage()
+    let cart = cargarCarritoDesdeLocalStorage();
+  
+    // MOSTRAR PRODUCTOS //
+  
+    function mostrarProductos() {
+      Products.forEach((Product) => {
+        let card = document.createElement("div");
+        showAllProducts.append(card);
+        let img = document.createElement("img");
+        img.setAttribute("src", Product.img);
+        let name = document.createElement("h3");
+        name.innerText = Product.name;
+        let price = document.createElement("p");
+        price.innerText = Product.price;
+        let btnAgregar = document.createElement("button");
+        btnAgregar.innerText = "Agregar";
+        card.append(img, name, price, btnAgregar);
+  
+        btnAgregar.addEventListener("click", function () {
+          cart.push(Product);
+          btnAgregar.addEventListener("click", guardarCarritoEnLocalStorage);
+          alert("Agregaste " + Product.name + " al carrito");
+  
+          div.innerHTML = "";
+          showCart();
+        });
+      });
+    }
+  
+    mostrarProductos();
+  
+    // MOSTRAR CARRITO//
+  
+    let cartView = document.getElementById("showProductsCart");
+    let goToCart = document.getElementById("goToCart");
+    // CARRITO ES UN ELEMENTO HTML!
+    let carrito = document.getElementById("mostrarCarrito");
+  
+    let alertCart = document.createElement("h2");
+    alertCart.setAttribute = ("class", "alerta");
+  
+    if (!cart.length) {
+      alertCart.innerText = "El carrito esta vacio";
+      div.append(alertCart);
+    }
+  
+    function showCart() {
+      alertCart.remove();
+  
+      cart.forEach((Product) => {
+        const divCart = document.createElement("li");
+        divCart.innerHTML += ` 
+          <img src = "${Product.img}">
+          <h3> ${Product.name}</h3>
+          <h3>$ ${Product.price}</h3>
+         `;
+  
+        div.appendChild(divCart);
+      });
+  
+    
+  
+      // REDUCE PRECIO TOTAL DEL CARRITO //
+  
+      const total = cart
+        .map((Product) => parseInt(Product.price))
+        .reduce(
+          (cartTotalPrice, currentProductPrice) =>
+            cartTotalPrice + currentProductPrice,
+          0
+        );
+      console.log(total);
+  
+      let totalCompra = document.createElement("h4");
+      totalCompra.innerText = "Total: " + total;
+      div.append(totalCompra);
+      
+      // Vaciar carrito //
+  
+      let deleteCart = document.createElement("button");
+      deleteCart.innerText = "Vaciar carrito";
+      div.append(deleteCart);
+  
+      deleteCart.onclick = () => {
+        cart = [];
+        div.innerHTML = ``;
+        console.log(cart);
+        
+        localStorage.clear();
+      };
+    }
+  
+  
+    // Guardar Lista //
+    let saveButton = document.getElementById("saveButton");
+    
+    saveButton.addEventListener("click", guardarCarritoEnLocalStorage);
+  
+    // Storage y JSON //
+  
+    function guardarCarritoEnLocalStorage() {
+      console.log("here");
+    
+      localStorage.setItem("cart", JSON.stringify(cart));
+     
+    }
+  
+    function cargarCarritoDesdeLocalStorage() {
+     
+      if (localStorage.getItem("cart") !== null) {
+        return JSON.parse(localStorage.getItem("cart"));
+      }
+    
+      return [];
+    }
+  
+    localStorage.setItem("Products", JSON.stringify(Products));
+  
 
-// localStorage.setItem("listaDeProductos", JSON.stringify(Products))
-
-})
+  
+    //Filtrar Productos //
+  
+    let buscador = document.getElementById("inputSearch");
+    let filtrar = document.getElementById("filtrar");
+  
+  
+    function filtrarPorCat() {
+      let Products = localStorage.getItem("favoritos");
+      let verTodo = document.createElement("button");
+      verTodo.innerText = "Ver Todo";
+      showProducts.append(verTodo);
+      const filteredProducts = Products.filter(
+        (Product) => Product.category === buscador.value
+      );
+      console.log(filteredProducts);
+  
+      filteredProducts.forEach((filter) => {
+        let cardFilter = document.createElement("div");
+        showAllProducts.append(card);
+        let imgFilter = document.createElement("img");
+        imgFilter.setAttribute("src", Product.img);
+        let nameFilter = document.createElement("h3");
+        nameFilter.innerText = Product.name;
+        let priceFilter = document.createElement("p");
+        priceFilter.innerText = Product.price;
+        let buyButtonFilter = document.createElement("button");
+        buyButtonFilter.innerText = "Agregar al carrito";
+        cardFilter.append(imgFilter, nameFilter, priceFilter, buyButtonFilter);
+      });
+  
+      verTodo.onclick = () => {
+        showAllProducts.innerHTML = ``;
+        mostrarProductos();
+      };
+    }
+  
+    buscador.onchange = () => {
+      showAllProducts.innerHTML = ``;
+      filtrarPorCat();
+    };
+  
+    filtrar.onclick = () => {
+      showAllProducts.innerHTML = ``;
+      filtrarPorCat();
+    };
+   
+  });
+  
